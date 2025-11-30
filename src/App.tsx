df7c56b3f07c1b6feaeb0e45f50ce5f3cc1ff620
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2025-11-30 14:15:18 +0200

    HW1 #gi01MZ6v

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
 
+const coursesTitleArray:string[] = [
+  'JavaScript Complex',
+  'Java Complex',
+  'Python Complex',
+  'QA Complex',
+  'Fullstack',
+  'Frontend'
+];
+
 function App() {
   return (
-    <>
-    </>
+    <ul>
+      {
+        coursesTitleArray.map((title, index) => (<li key={index}>{title}</li>))
+      }
+    </ul>
   )
 }
 
