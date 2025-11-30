7f8c51be4cc1de845c59d3652c96a07f04c98655
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2025-11-30 23:00:54 +0200

    HW1 #Fs05Kiho - start

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css'
+import {coursesAndDurationArray} from "./data/coursesList.ts";
 
 function App() {
   return (
     <>
+      {
+        coursesAndDurationArray.map((item, i) =>
+          <p key={i}> {item.title} </p>
+        )
+      }
     </>
   )
 }
