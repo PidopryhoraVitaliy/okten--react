3670ce799d0a795a8db128564802d14276e18253
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2026-01-25 23:01:05 +0200

    HW3 #chszHaH9 - BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
-import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {routes} from "./router/routes.tsx";
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {UsersPage} from "./pages/UsersPage.tsx";
+import {PostsPage} from "./pages/PostsPage.tsx";
+import {CommentsPage} from "./pages/CommentsPage.tsx";
+import {ProductsPage} from "./pages/ProductsPage.tsx";
+import {Layout} from "./layouts/Layout.tsx";
+import {MainPage} from "./pages/MainPage.tsx";
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>);
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<Layout/>}>
+                {/*<Route path={'/'} element={<MainPage/>}/>*/}
+                <Route index={true} element={<MainPage/>}/>
+                <Route path={'/users'} element={<UsersPage/>}/>
+                <Route path={'/posts'} element={<PostsPage/>}/>
+                <Route path={'/comments'} element={<CommentsPage/>}/>
+                <Route path={'/products'} element={<ProductsPage/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>);
