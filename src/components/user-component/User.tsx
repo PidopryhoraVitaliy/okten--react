5b8d8f3d67a36660b14ef3d18709500e8cb4368f
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2026-02-11 21:55:46 +0200

    hw4 - add index

--- src/components/user-component/User.tsx
+++ src/components/user-component/User.tsx
@@ -1,5 +1,5 @@
 import './User.css';
-import type {FC} from "react";
+import {type FC} from "react";
 import type {UserModel} from "../../models/UserModel.ts";
 
 type PropType = { user: UserModel; }
@@ -7,6 +7,7 @@ type PropType = { user: UserModel; }
 export const User: FC<PropType> = ({user}) => {
     return (
         <div className={'user'}>
+            <b>{user.id}</b>
             <p>user: <b>{user.firstName} {user.lastName}</b> ({user.username})</p>
             <p>phone: {user.phone}</p>
             <p>email: {user.email}</p>
