23d7f214bea9d3c25fb9af4ad8c4bdfc5f843174
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2026-03-23 22:32:36 +0200

    hw8 - added user detail information

--- src/components/user-component/User.tsx
+++ src/components/user-component/User.tsx
@@ -1,6 +1,7 @@
 import './User.css';
 import {type FC} from "react";
 import type {IUser} from "../../models/IUser.ts";
+import {Link} from "react-router-dom";
 
 type PropType = { user: IUser; }
 
@@ -9,6 +10,7 @@ export const User: FC<PropType> = ({user}) => {
         <div className={'user'}>
             <b>{user.id}</b>
             <p>user: <b>{user.name}</b></p>
+            <Link to={`./${user.id}`}>detail information</Link>
         </div>
     );
 };
